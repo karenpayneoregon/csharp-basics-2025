6ce4343f028f509bcea9233e205674d4820ae1ad
Karen Payne <paynekaren@comcast.net>
2025-11-20 20:10:20 -0800

    Update to .NET 9 and integrate Spectre.Console
    Updated the target framework to .NET 9 in `ConsoleApp1.csproj`
    and added a reference to the `Spectre.Console` library for
    enhanced console interactions. Introduced a project reference
    to `SpectreConsoleLibrary.csproj`. Modified `Program.cs` to
    replace `Console.WriteLine` with `Console.ReadLine` and removed
    commented-out code for date extraction. Added a new folder
    `Extra\` in `SamplesApp.csproj` for additional resources.
    Moved the date extraction code to `Dump.txt` for reference.
    
    by Karen

--- SamplesApp/Program.cs
+++ SamplesApp/Program.cs
@@ -12,19 +12,6 @@ internal partial class Program
     static void Main(string[] args)
     {
         BasicStatements.IfBirthYearStatementBasic();
-
-
-        //var line = "\\\\SomeServer\\HTTP\\FolderName\\data.txt 4 KB TXT " +
-        //           "File 02/19/2019 3:48:21 PM " +
-        //           "2/19/2019 1:05:53 PM 2/19/2019 1:05:53 PM 5";
-
-        //var dates = DateTimeExtractor.GetAllDateTimes(line);
-
-        //foreach (var d in dates)
-        //{
-        //    Console.WriteLine(d);
-        //}
-        
         SpectreConsoleHelpers.ExitPrompt();
     }
 
